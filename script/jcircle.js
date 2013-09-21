32e399453c4925c681c960cfcd5779bee095b9f1
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-09-21 16:31:07 +0100

    Initial Commit

---
+++ script/jcircle.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+function start_j_circle() {
+	console.log('Hello, I\'m jCirle');
+	build_list('I love my ass fucked');
+}
+var bum = 0;
+function build_list (poo) {
+	console.log(poo);
+		$('.jcircle').children().each(function() {
+			bum++;
+			console.log('You have acknokledged ' + bum + 'items');
+		});
+	
+}

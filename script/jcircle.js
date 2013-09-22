0ee21417207527a33abd37f117bac5e6dd30180d
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-09-22 18:18:37 +0100

    refactor

--- script/jcircle.js
+++ script/jcircle.js
@@ -3,13 +3,34 @@
 function start_j_circle() {
 	console.log('Hello, I\'m jCirle');
 	build_list('I love my ass fucked');
+	//
 }
-var bum = 0;
+var itemNo = 0;
 function build_list (poo) {
-	console.log(poo);
-		$('.jcircle').children().each(function() {
-			bum++;
-			console.log('You have acknokledged ' + bum + 'items');
+	console.log(poo);	
+	$('.jcircle').append('<div class="view-port"></div>');
+	$('.jcircle').append('<ul class="jc-list"></ul>');
+		$('.jcircle').children().each(function(e) {
+			var imageUrl = $(this).attr('src');
+			if(imageUrl) {
+				itemNo++;
+				$('<li></li>').appendTo('.jc-list').addClass('thumbs').addClass('shadow').attr('image',imageUrl).attr('item','itemNo_' + itemNo).css('background-image', 'url(' + imageUrl + ')');
+				console.log('You have acknowledged ' + itemNo + ' items and:' + imageUrl);
+				$(this).remove();
+			}
 		});
+	$('.thumbs').bind('click',function() {
+		load_main(this)
+	});
+}
+
+function load_main(data) {
+	var itemInfo = $(data).attr('item');
+	var itemImage = $(data).attr('image');
+	$('.view-port').html('<img src="' + itemImage +'"/>').addClass('main_image');
+	$('.view-port img').addClass('shadow');
+	console.log(itemInfo);
 	
 }
+	
+	

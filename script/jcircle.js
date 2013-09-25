37f618fd6b49574584e1f23a1ee0c3d8cb2b9150
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-09-25 19:03:40 +0100

    Refactor

--- script/jcircle.js
+++ script/jcircle.js
@@ -12,10 +12,14 @@ function build_list (poo) {
 	$('.jcircle').append('<ul class="jc-list"></ul>');
 		$('.jcircle').children().each(function(e) {
 			var imageUrl = $(this).attr('src');
-			if(imageUrl) {
-				itemNo++;
-				$('<li></li>').appendTo('.jc-list').addClass('thumbs').addClass('shadow').attr('image',imageUrl).attr('item','itemNo_' + itemNo).css('background-image', 'url(' + imageUrl + ')');
-				console.log('You have acknowledged ' + itemNo + ' items and:' + imageUrl);
+			if(imageUrl) {		
+				itemNo++;				 
+				$(this).attr('id','imageid_' + itemNo);		
+				var img = document.getElementById('imageid_' + itemNo); 
+				var width = img.clientWidth;
+				$('<li id="img_list_'+ itemNo +'"></li>').appendTo('.jc-list').addClass('thumbs').addClass('shadow').attr('image',imageUrl).attr('item','itemNo_' + itemNo).css('background-image', 'url(' + imageUrl + ')');
+				$('<img src="'+ imageUrl +'" />').appendTo('#img_list_' + itemNo).addClass('thumbs');
+				console.log('You have acknowledged ' + itemNo + ' items and:' + imageUrl + "Width is: " + width);
 				$(this).remove();
 			}
 		});
@@ -27,9 +31,17 @@ function build_list (poo) {
 function load_main(data) {
 	var itemInfo = $(data).attr('item');
 	var itemImage = $(data).attr('image');
-	$('.view-port').html('<img src="' + itemImage +'"/>').addClass('main_image');
-	$('.view-port img').addClass('shadow');
-	console.log(itemInfo);
+	var itemPos = $(data);
+	var position = itemPos.position();
+	$(data).addClass('absoluted').css({left:position.left}).animate({
+		bottom: "85px",
+		left: "200px",
+		height: "300px",
+		width: "300px"
+		}, 500);
+	//$('.view-port').html('<img src="' + itemImage +'"/>').addClass('main_image');
+	//$('.view-port img').addClass('shadow');
+	console.log(itemInfo + " ");
 	
 }
 	
